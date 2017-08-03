b7ff632f97a7832919e876e4d0918222a5ba956f
Ali Azlan <ali.azlan@RT-DT-519.rolustech.net>
2017-08-03 16:18:24 +0500

    fresh install

---
+++ modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2017-08-03 16:12:52
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Prospects' => 
+  array (
+    'ext_rest_twitter' => 'ext_rest_twitter',
+  ),
+  'Opportunities' => 
+  array (
+  ),
+);
