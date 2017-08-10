ac5d7d0844d66b9076b3ec2acf46d66f3263737b
Ali Azlan <ali.azlan@RT-DT-519.rolustech.net>
2017-08-10 18:19:46 +0500

    4th article done

---
+++ Extension/modules/Administration/Ext/Language/en_us.example.php
@@ -0,0 +1,6 @@
+<?php
+
+    $mod_strings['LBL_LINK_NAME'] = 'Example Link';
+    $mod_strings['LBL_LINK_DESCRIPTION'] = 'Link Description';
+    $mod_strings['LBL_SECTION_HEADER'] = 'Example Admin Panel';
+    $mod_strings['LBL_SECTION_DESCRIPTION'] = 'Section Description';
