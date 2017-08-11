ebc1bb4adeeff80b01ef1e640fa81962a6b74a13
Ali Azlan <ali.azlan@RT-DT-519.rolustech.net>
2017-08-11 17:41:49 +0500

    FileAccessControlMap

---
+++ modules/m1234_Student/Ext/FileAccessControlMap/file_access_control_map.ext.php
@@ -0,0 +1,12 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/modules/m1234_Student/Ext/FileAccessControlMap/example.php
+
+
+$file_access_control_map['modules']['m1234_Student']['actions'] = array(
+    'detailview',
+);
+
+?>
