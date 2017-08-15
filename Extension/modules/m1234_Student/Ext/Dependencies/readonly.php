da27ff411625285a99c00522f89ba4fb9748cca1
Ali Azlan <ali.azlan@RT-DT-519.rolustech.net>
2017-08-15 18:21:40 +0500

    readonly action implimented

--- Extension/modules/m1234_Student/Ext/Dependencies/readonly.php
+++ Extension/modules/m1234_Student/Ext/Dependencies/readonly.php
@@ -1,34 +1,4 @@
 <?php
-// WARNING: The contents of this file are auto-generated.
-?>
-<?php
-// Merged from custom/Extension/modules/m1234_Student/Ext/Dependencies/example.php
-
-
-$dependencies['m1234_Student']['example_depn'] = array(
-'hooks' => array("all"),
-//Trigger formula for the dependency. Defaults to 'true'.
-'trigger' => 'true',
-'triggerFields' => array('first_name'),
-'onload' => true,
-//Actions is a list of actions to fire when the trigger is true
-'actions' => array(
-    array(
-        'name' => 'SetRequired', //Action type
-        //The parameters passed in depend on the action type
-        'params' => array(
-            'target' => 'phone_mobile',
-            'label'  => 'LBL_MOBILE_PHONE', //normally <field>_label
-            'value' => 'equal($first_name, "Closed")', //Formula
-        ),
-    ),
-),
-);
-
-?>
-<?php
-// Merged from custom/Extension/modules/m1234_Student/Ext/Dependencies/readonly.php
-
 
     $dependencies['m1234_Student']['readonly_fields'] = array(
         'hooks' => array("edit"),
@@ -59,5 +29,3 @@ $dependencies['m1234_Student']['example_depn'] = array(
             ),
         ),
     );
-
-?>
