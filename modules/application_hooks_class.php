c6daa287080f709fc838bdb9127d97d2ba449d93
Ali Azlan <ali.azlan@RT-DT-519.rolustech.net>
2017-08-18 10:43:47 +0500

    yesterdays work. application logic hooks

---
+++ modules/application_hooks_class.php
@@ -0,0 +1,33 @@
+<?php
+
+    if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+    class application_hooks_class
+    {
+        function after_entry_point_method($event, $arguments)
+        {
+            //logic
+        }
+
+        function after_load_user_method($bean, $event, $arguments)
+        {
+            //logic
+        }
+
+        function after_session_start_method($bean, $event, $arguments)
+        {
+            //logic
+        }
+
+        function handle_exception_method($event, $exception)
+        {
+            $exception->getMessage();
+        }
+
+        function server_round_trip_method($event, $arguments)
+        {
+            //display logic should check for $_REQUEST["to_pdf"]
+        }
+    }
+
+?>
---
+++ modules/logic_hooks.php
@@ -0,0 +1,97 @@
+<?php
+
+    $hook_version = 1;
+    $hook_array = Array();
+
+    $hook_array['after_entry_point'] = Array();
+    $hook_array['after_entry_point'][] = Array(
+        //Processing index. For sorting the array.
+        1,
+
+        //Label. A string value to identify the hook.
+        'after_entry_point example',
+
+        //The PHP file where your class is located.
+        'custom/modules/application_hooks_class.php',
+
+        //The class the method is in.
+        'application_hooks_class',
+
+        //The method to call.
+        'after_entry_point_method'
+    );
+
+    $hook_array['after_load_user'] = Array();
+    $hook_array['after_load_user'][] = Array(
+        //Processing index. For sorting the array.
+        1,
+
+        //Label. A string value to identify the hook.
+        'after_load_user example',
+
+        //tde PHP file where your class is located.
+        'custom/modules/application_hooks_class.php',
+
+        //tde class the method is in.
+        'application_hooks_class',
+
+        //tde method to call.
+        'after_load_user_method'
+    );
+
+    $hook_array['after_session_start'] = Array();
+    $hook_array['after_session_start'][] = Array(
+        //Processing index. For sorting the array.
+        1,
+
+        //Label. A string value to identify the hook.
+        'after_session_start example',
+
+        //The PHP file where your class is located.
+        'custom/modules/application_hooks_class.php',
+
+        //The class the method is in.
+        'application_hooks_class',
+
+        //The method to call.
+        'after_session_start_method'
+    );
+
+    $hook_array['handle_exception'] = Array();
+    $hook_array['handle_exception'][] = Array(
+        //Processing index. For sorting the array.
+        1,
+
+        //Label. A string value to identify the hook.
+        'handle_exception example',
+
+        //The PHP file where your class is located.
+        'custom/modules/handle_exception_class.php',
+
+        //The class the method is in.
+        'handle_exception_class',
+
+        //The method to call.
+        'handle_exception_method'
+    );
+
+    $hook_array['server_round_trip'] = Array();
+    $hook_array['server_round_trip'][] = Array(
+        //Processing index. For sorting the array.
+        1,
+
+        //Label. A string value to identify the hook.
+        'server_round_trip example',
+
+        //The PHP file where your class is located.
+        'custom/modules/application_hooks_class.php',
+
+        //The class the method is in.
+        'application_hooks_class',
+
+        //The method to call.
+        'server_round_trip_method'
+    );
+
+
+?>
